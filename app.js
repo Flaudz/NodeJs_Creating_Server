16ab65f51a7944017284e3c7d84a39aa6a95de6c
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-14 14:29:54 +0100

    Set a response and made port number + localhost(127.0.0.1)

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ const http = require("http");
 
 let server = http.createServer((req, res) => {
 	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.end("Hey ninjas");
 });
+
+server.listen(5050, "127.0.0.1");
