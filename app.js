fbb639ddbb070186193e2adde18f4b98babf2b08
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-14 14:35:23 +0100

    Made so you can see the req url

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
 let server = http.createServer((req, res) => {
+	console.log(`Request was me: ${req.url}`);
 	res.writeHead(200, { "Content-Type": "text/plain" });
 	res.end("Hey ninjas");
 });
